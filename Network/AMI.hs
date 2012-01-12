dab119539a97625601df7e6ed377e02e466a2b70
Ilya V. Portnov <portnov@iportnov.ru>
2012-01-12 16:57:28 +0600

    Try to support text responses.

--- Network/AMI.hs
+++ Network/AMI.hs
@@ -27,7 +27,7 @@ type ResponseHandler = Packet -> AMI ()
 
 data Packet =
     Action ActionID ActionType Parameters
-  | Response ActionID ResponseType Parameters
+  | Response ActionID ResponseType Parameters [B.ByteString]
   | Event EventType Parameters
   deriving (Eq, Show)
 
@@ -83,7 +83,7 @@ wait = do
   case parse str of
     Left err -> fail err
     Right (Action {}) -> fail "Unexpected Action from server"
-    Right p@(Response i t _) -> do
+    Right p@(Response i t _ _) -> do
         st <- get
         case M.lookup i (amiResponseHandlers st) of
           Nothing -> liftIO $ putStrLn $ "No handler for " ++ show i
@@ -106,7 +106,7 @@ open info = do
     wait
   where
     handleAuth :: Packet -> AMI ()
-    handleAuth (Response _ "Success" _) = return ()
+    handleAuth (Response _ "Success" _ _) = return ()
     handleAuth _ = fail "Authentication failed"
 
 close :: AMI ()
@@ -143,20 +143,24 @@ linesB y = h : if B.null t then [] else linesB (B.drop 2 t)
    where (h,t) = B.breakSubstring "\r\n" y
 
 parse :: B.ByteString -> Either String Packet
-parse str = (toPacket . concat) =<< (mapM toPair $ B.split '\r' str)
+parse str = uncurry toPacket =<< (toPairs [] $ B.split '\r' str)
   where
-    toPair :: B.ByteString -> Either String [(B.ByteString, B.ByteString)]
-    toPair s = case B.split ':' s of
-                 []     -> Right []
-                 [n,v]  -> Right [(n, B.dropWhile (== ' ') v)]
-                 x      -> Left $ "Unexpected: " ++ show x
+    toPairs :: Parameters -> [B.ByteString] -> Either String (Parameters, [B.ByteString])
+    toPairs [] [] = Left "Empty packet"
+    toPairs acc [] = Right (acc, [])
+    toPairs acc (s:ss) =
+      case B.split ':' s of
+        []     -> return (acc, [])
+        [n,v]  -> let new = (n, B.dropWhile (== ' ') v)
+                  in  toPairs (new:acc) ss
+        x      -> Right (acc, (s:ss))
 
-    toPacket :: Parameters -> Either String Packet
-    toPacket [] = Left "Empty packet!?"
-    toPacket ((k,v):pairs) =
+    toPacket :: Parameters -> [B.ByteString] -> Either String Packet
+    toPacket [] _ = Left "Empty packet!?"
+    toPacket ((k,v):pairs) text =
       case k of
         "Action"   -> toAction   v pairs
-        "Response" -> toResponse v pairs
+        "Response" -> toResponse v pairs text
         "Event"    -> toEvent    v pairs
         _          -> Left $ "Invalid first parameter: " ++ show v
 
@@ -172,15 +176,16 @@ parse str = (toPacket . concat) =<< (mapM toPair $ B.split '\r' str)
       (i, ps) <- getField "ActionID" pairs
       return $ Action i name ps
 
-    toResponse name pairs = do
+    toResponse name pairs text = do
       (i, ps) <- getField "ActionID" pairs
-      return $ Response i name ps
+      return $ Response i name ps text
 
     toEvent name pairs = Right $ Event name pairs
 
 format :: Packet -> B.ByteString
 format (Action i name ps)   = formatParams $ [("Action", name), ("ActionID", i)] ++ ps
-format (Response i name ps) = formatParams $ [("Response", name), ("ActionID", i)] ++ ps
+format (Response i name ps text) =
+    formatParams ([("Response", name), ("ActionID", i)] ++ ps) `B.append` "\r\n" `B.append` B.intercalate "\r\n" text
 format (Event name ps)      = formatParams $ [("Event", name)] ++ ps
 
 formatParams :: Parameters -> B.ByteString
